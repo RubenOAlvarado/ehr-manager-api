ea21403c04ab4c9dc41e600b5f606f6c55a09bdc
Ruben Alvarado <ruben.alvardo@plexusworldwide.com>
2025-05-09 11:08:18 -0600

    refactor: comment out CORS configuration for debugging
    The CORS configuration in `main.ts` has been temporarily commented out to facilitate debugging and testing. This change does not affect the core functionality but allows for easier isolation of issues during development.

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
+  /* app.enableCors({
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
@@ -17,7 +17,7 @@ async function bootstrap() {
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-  });
+  }); */
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.setGlobalPrefix(API_BASE_PATH);
   await buildSwaggerModule(app);
