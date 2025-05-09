a8e989ed2465fb2cf6bb38228affb862f77960b4
Ruben Alvarado <ruben.alvardo@plexusworldwide.com>
2025-05-09 11:19:07 -0600

    feat(cors): enable CORS with additional allowed origins
    Add 'https://patients-site.vercel.app' and 'https://admin-site-phi.vercel.app' to the list of allowed origins for CORS to support frontend deployments on Vercel.

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,19 @@ import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  /* app.enableCors({
+  app.enableCors({
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
       'http://caddy',
       'http://localhost',
       'http://127.0.0.1',
+      'https://patients-site.vercel.app',
+      'https://admin-site-phi.vercel.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-  }); */
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.setGlobalPrefix(API_BASE_PATH);
   await buildSwaggerModule(app);
